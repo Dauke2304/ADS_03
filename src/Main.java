0b18e463c0fef61c70ebbc598a0d2e64ce97667f
20fut <231612@astanait.edu.kz>
2024-05-15 13:02:20 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Random;
+import Main.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkHashTable();
     }
-
+    public static void checkHashTable() {
+        Random random = new Random();
+        int size = 10000;
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(size);
+        for (int i = 0; i < size; i++) {
+            MyTestingClass test = new MyTestingClass(String.valueOf(i),String.valueOf(i));
+            Student student = new Student(String.valueOf(i),String.valueOf(i), 1.0 + (random.nextDouble() * 3));
+            table.put(test,student);
+        }
+        int qw = 0;
+        for (int i = 0; i < size; i++) {
+            MyTestingClass test = new MyTestingClass(String.valueOf(i), String.valueOf(i));
+            if (table.get(test) == null) {
+                qw++;
+                System.out.println("Value not found: " + test);
+            }
+        }
+        System.out.println(qw);
+    }
 }
