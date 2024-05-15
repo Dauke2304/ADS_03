b00c1a78a20f3923ae8d31475cd4aeef727f1de8
20fut <231612@astanait.edu.kz>
2024-05-15 11:17:19 +0500

    1

--- src/Main/Student.java
+++ src/Main/Student.java
@@ -1,4 +1,37 @@
 package Main;
 
 public class Student {
+    private String barcode;
+    private String name;
+    private double gpa;
+
+    public Student(String barcode, String name, double gpa) {
+        this.barcode = barcode;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
