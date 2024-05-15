849654f9aa845afda56c7c3f34d5c95f7e822e0f
20fut <231612@astanait.edu.kz>
2024-05-15 11:36:40 +0500

    1

--- src/Main/BST.java
+++ src/Main/BST.java
@@ -1,4 +1,15 @@
 package Main;
 
 public class BST {
+    public Node root;
+
+    public class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
 }
--- src/Main/MyHashTable.java
+++ src/Main/MyHashTable.java
@@ -1,4 +1,50 @@
 package Main;
 
-public class MyHashTable {
+public class MyHashTable<K,V> {
+    private class HashNode<K,V> {
+        private K key;
+        private V value;
+        private HashNode<K,V> next;
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K,V>[] chainArray;
+    private static int M = 11;
+    private int size;
+    public MyHashTable() {
+        this(M);
+    }
+    public MyHashTable(int M) {
+
+    }
+
+    public int hash(K key) {
+
+    }
+    private int hashCode(K key) {
+
+    }
+    public void put(K key, V value) {
+
+    }
+    public V get(K key) {
+
+    }
+    public V remove(K key) {
+
+    }
+
+    public boolean contains(V value) {
+
+    }
+    public K getKey(V value) {
+
+    }
 }
