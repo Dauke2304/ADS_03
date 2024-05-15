605eeaeb87974e37c5bd213efa1ea1e2cecc7d49
20fut <231612@astanait.edu.kz>
2024-05-15 12:39:38 +0500

    1

--- src/Main/BST.java
+++ src/Main/BST.java
@@ -1,15 +1,45 @@
 package Main;
 
-public class BST {
-    public Node root;
+public class BST<K extends Comparable<K>, V>{
+    private Node root;
+    private int size;
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
 
-    public class Node {
-        int data;
-        Node left, right;
-
-        public Node(int data) {
-            this.data = data;
-            left = right = null;
+        public V getValue() {
+            return value;
         }
     }
+
+    public BST() {
+        root = null;
+    }
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.value = val;
+        }
+        return node;
+    }
+
 }
